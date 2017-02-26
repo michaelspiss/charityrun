df9184f761dcd50b506b9ab34c0e5abeef78064e
michaspiss <michael.spiss@web.de>
2017-02-26 23:49:40 +0100

    Add LoginCheckMiddleware

---
+++ app/src/Auth/LoginCheckMiddleware.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Auth;
+
+class LoginCheckMiddleware {
+    /**
+     * Example middleware invokable class
+     *
+     * @param  \Psr\Http\Message\ServerRequestInterface $request  PSR7 request
+     * @param  \Psr\Http\Message\ResponseInterface      $response PSR7 response
+     * @param  callable                                 $next     Next middleware
+     *
+     * @return \Psr\Http\Message\ResponseInterface
+     */
+    public function __invoke($request, $response, $next) {
+        // check if user is not logged in
+        if(!app('auth')->loggedIn()) {
+            // redirect returns a response, so this is fine
+            return redirect('/login');
+        }
+        // else just keep going
+        $response = $next($request, $response);
+        return $response;
+    }
+}
