8eb80d81e94f10f22544d4fb9cec137e2b9cbd41
michaspiss <michael.spiss@web.de>
2017-02-26 13:16:18 +0100

    Add single pages

--- app/routes.php
+++ app/routes.php
@@ -1,10 +1,56 @@
 <?php
-// Routes
 
-$app->get('/[{name}]', function ($request, $response, $args) {
-    // Sample log message
-    $this->logger->info("Slim-Skeleton '/' route");
+/*
+|--------------------------------------------------------------------------
+| SINGLE PAGES
+|--------------------------------------------------------------------------
+*/
 
-    // Render index view
-    return view($response, 'index', $args);
+/*
+ * Index, let the user choose what to do:
+ * 1. Display map
+ * 2. Display stats
+ * 3. Log in
+ */
+$app->get('/', function ($request, $response) {
+    // /
+});
+
+/*
+ * Display the live map, with statistics about the run
+ */
+$app->get('map', function ($request, $response) {
+    // /map
+});
+
+/*
+ * Display the current top-runners and overall statistics
+ * "Leaderboard"
+ */
+$app->get('stats', function ($request, $response) {
+    // /stats
+});
+
+/*
+ * Display log in form, redirect if already logged in
+ */
+$app->get('login', function ($request, $response) {
+    // /login
+});
+
+/*
+ * If password is correct authenticate user and redirect to /manage,
+ * else send back to login
+ */
+$app->post('login', function ($request, $response) {
+    // POST: /login
+    // password verification, user authentication
+});
+
+/*
+ * Log the user out, redirect to index
+ */
+$app->get('logout', function ($request, $response) {
+    // POST: /logout
+    // log out user
 });
