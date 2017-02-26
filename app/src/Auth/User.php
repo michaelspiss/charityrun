78b21976f34a8dd93264b382c6ec539cd10fa3b5
michaspiss <michael.spiss@web.de>
2017-02-26 22:39:19 +0100

    Add class User

---
+++ app/src/Auth/User.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Auth;
+
+use Solution10\Auth\UserRepresentation;
+
+class User implements UserRepresentation {
+
+    protected $id;
+    protected $username;
+
+    function __construct($id, $username)
+    {
+        $this->id = $id;
+        $this->username = $username;
+    }
+
+    /**
+     * Returns the ID of this user. Doesn't matter what type it returns.
+     * @return    mixed
+     */
+    public function id()
+    {
+        return $this->id;
+    }
+}
