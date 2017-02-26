1f34da939b4d81cce685bb089532bec7b9d0ecd2
michaspiss <michael.spiss@web.de>
2017-02-26 13:22:57 +0100

    Add /manage pages

--- app/routes.php
+++ app/routes.php
@@ -54,3 +54,78 @@ $app->get('logout', function ($request, $response) {
     // POST: /logout
     // log out user
 });
+
+/*
+|--------------------------------------------------------------------------
+| MANAGE
+|--------------------------------------------------------------------------
+*/
+
+/*
+ * Display a list of all classes to choose from
+ */
+$app->get('manage', function ($request, $response) {
+    // /manage
+});
+
+/*
+ * Get class selection, redirect to /manage/{class},
+ * where class is the selected class.
+ * This will only be called if JavaScript is disabled
+ */
+$app->post('manage', function ($request, $response) {
+    // POST: /manage
+});
+
+/*
+ * Display a FAQ page regarding the management of classes
+ */
+$app->get('manage/help', function ($request, $response) {
+    // /manage/help
+});
+
+$app->group('manage/{class}', function () {
+
+    /*
+    * Display an overview of the selected class (runners, finished rounds)
+    */
+    $this->get('/', function ($request, $response, $class) {
+        // /manage/{class}
+    });
+
+    /*
+    * Interface to add rounds to the runners
+    */
+    $this->get('add', function ($request, $response, $class) {
+        // /manage/{class}/add
+    });
+
+    /*
+     * Add rounds to the runners
+     */
+    $this->post('add', function ($request, $response, $class) {
+        // POST: /manage/{class}/add
+    });
+
+    /*
+     * Display log of all recent updates of the runners rounds
+     */
+    $this->get('log', function ($request, $response, $class) {
+        // /manage/{class}/log
+    });
+
+    /*
+    * Undo round updates, Undo undos
+    */
+    $this->post('log', function ($request, $response, $class) {
+        // POST: /manage/{class}/log
+    });
+
+    /*
+    * Display sidebar, from "more"
+    * This will only be called if JavaScript is disabled
+    */
+    $this->get('more', function ($request, $response, $class) {
+        // /manage/{class}/more
+    });
+});
